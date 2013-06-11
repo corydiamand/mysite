f9242e19368b2b81754646373c77a9fba3e276b9
Cory Diamand <cory@postpourri.com>
2013-06-11 09:25:53 -0400

    styling text, change posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130609020015) do
+ActiveRecord::Schema.define(:version => 20130611132058) do
 
   create_table "posts", :force => true do |t|
-    t.string   "name"
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", :null => false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create([{ title: 'And then there was blog!',content: 'Soc. Then will they not say: "You, Socrates, are breaking the covenants and agreements which you made with us at your leisure, not in any haste or under any compulsion or deception, but having had seventy years to think of them, during which time you were at liberty to leave the city, if we were not to your mind, or if our covenants appeared to you to be unfair. You had your choice, and might have gone either to Lacedaemon or Crete, which you often praise for their good government, or to some other Hellenic or foreign State. Whereas you, above all other Athenians, seemed to be so fond of the State, or, in other words, of us her laws (for who would like a State that has no laws?), that you never stirred out of her: the halt, the blind, the maimed, were not more stationary in her than you were. And now you run away and forsake your agreements. Not so, Socrates, if you will take our advice; do not make yourself ridiculous by escaping out of the city. 
+
+"For just consider, if you transgress and err in this sort of way, what good will you do, either to yourself or to your friends? That your friends will be driven into exile and deprived of citizenship, or will lose their property, is tolerably certain; and you yourself, if you fly to one of the neighboring cities, as, for example, Thebes or Megara, both of which are well-governed cities, will come to them as an enemy, Socrates, and their government will be against you, and all patriotic citizens will cast an evil eye upon you as a subverter of the laws, and you will confirm in the minds of the judges the justice of their own condemnation of you. For he who is a corrupter of the laws is more than likely to be corrupter of the young and foolish portion of mankind. Will you then flee from well-ordered cities and virtuous men? And is existence worth having on these terms? Or will you go to them without shame, and talk to them, Socrates? And what will you say to them? What you say here about virtue and justice and institutions and laws being the best things among men? Would that be decent of you? Surely not. But if you go away from well-governed States to Critos friends in Thessaly, where there is great disorder and license, they will be charmed to have the tale of your escape from prison, set off with ludicrous particulars of the manner in which you were wrapped in a goatskin or some other disguise, and metamorphosed as the fashion of runaways is- that is very likely; but will there be no one to remind you that in your old age you violated the most sacred laws from a miserable desire of a little more life? Perhaps not, if you keep them in a good temper; but if they are out of temper you will hear many degrading things; you will live, but how?- as the flatterer of all men, and the servant of all men; and doing what?- eating and drinking in Thessaly, having gone abroad in order that you may get a dinner. And where will be your fine sentiments about justice and virtue then? Say that you wish to live for the sake of your children, that you may bring them up and educate them- will you take them into Thessaly and deprive them of Athenian citizenship? Is that the benefit which you would confer upon them? Or are you under the impression that they will be better cared for and educated here if you are still alive, although absent from them; for that your friends will take care of them? Do you fancy that if you are an inhabitant of Thessaly they will take care of them, and if you are an inhabitant of the other world they will not take care of them? Nay; but if they who call themselves friends are truly friends, they surely will. 
+
+"Listen, then, Socrates, to us who have brought you up. Think not of life and children first, and of justice afterwards, but of justice first, that you may be justified before the princes of the world below. For neither will you nor any that belong to you be happier or holier or juster in this life, or happier in another, if you do as Crito bids. Now you depart in innocence, a sufferer and not a doer of evil; a victim, not of the laws, but of men. But if you go forth, returning evil for evil, and injury for injury, breaking the covenants and agreements which you have made with us, and wronging those whom you ought least to wrong, that is to say, yourself, your friends, your country, and us, we shall be angry with you while you live, and our brethren, the laws in the world below, will receive you as an enemy; for they will know that you have done your best to destroy us. Listen, then, to us and not to Crito." 
+
+This is the voice which I seem to hear murmuring in my ears, like the sound of the flute in the ears of the mystic; that voice, I say, is humming in my ears, and prevents me from hearing any other. And I know that anything more which you will say will be in vain. Yet speak, if you have anything to say.' }])
+
+Post.create([{ title: "A second post is made", content: 'Soc. But they will be sure to listen if they find that you are a
+good speaker. There was a notion that came into my mind while you were
+speaking; I said to myself: "Well, and what if Euthyphro does prove to
+me that all the gods regarded the death of the serf as unjust, how
+do I know anything more of the nature of piety and impiety? for
+granting that this action may be hateful to the gods, still piety
+and impiety are not adequately defined by these distinctions, for that
+which is hateful to the gods has been shown to be also pleasing and
+dear to them." And therefore, Euthyphro, I do not ask you to prove
+this; I will suppose, if you like, that all the gods condemn and
+abominate such an action. But I will amend the definition so far as to
+say that what all the gods hate is impious, and what they love pious
+or holy; and what some of them love and others hate is both or
+neither. Shall this be our definition of piety and impiety?
+
+  Euth. Why not, Socrates?
+
+  Soc. Why not! certainly, as far as I am concerned, Euthyphro,
+there is no reason why not. But whether this admission will greatly
+assist you in the task of instructing me as you promised, is a
+matter for you to consider.'}])
